a27dcb92dba242ff00c2dfea95f91cfa1601cc0a
Manuel Barkhau <mbarkhau@gmail.com>
2018-11-29 16:47:48 +0100

    add bootstrapit.sh

---
+++ bootstrapit.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Bootstrapit Project Configuration
+
+AUTHOR_NAME="Manuel Barkhau"
+AUTHOR_CONTACT="@mbarkhau"
+
+KEYWORDS="environ variables mypy config configuration"
+DESCRIPTION="Environment Variable Parsing with type annotations."
+
+
+LICENSE_ID="MIT"
+
+PACKAGE_NAME="myenv"
+MODULE_NAME="myenv"
+GIT_REPO_NAMESPACE="mbarkhau"
+GIT_REPO_DOMAIN="gitlab.com"
+
+DEFAULT_PYTHON_VERSION="python=3.6"
+
+# GIT_REPO_URL=https://${GIT_REPO_DOMAIN}/${GIT_REPO_NAMESPACE}/${PACKAGE_NAME}
+
+
+IS_PUBLIC=1
+
+
+## Download and run the actual update script
+
+if [[ $KEYWORDS == "keywords used on pypi" ]]; then
+    echo "Default bootstrapit config detected.";
+    echo "Did you forget to update parameters in your 'bootstrapit.sh' ?"
+    exit 1;
+fi
+
+PROJECT_DIR=$(dirname "$0");
+
+if ! [[ -f "$PROJECT_DIR/scripts/bootstrapit_update.sh" ]]; then
+    mkdir -p "$PROJECT_DIR/scripts/";
+    RAW_FILES_URL="https://gitlab.com/mbarkhau/bootstrapit/raw/master";
+    curl --silent "$RAW_FILES_URL/scripts/bootstrapit_update.sh" \
+        > "$PROJECT_DIR/scripts/bootstrapit_update.sh.tmp";
+    mv "$PROJECT_DIR/scripts/bootstrapit_update.sh.tmp" \
+        "$PROJECT_DIR/scripts/bootstrapit_update.sh";
+fi
+
+source "$PROJECT_DIR/scripts/bootstrapit_update.sh";
