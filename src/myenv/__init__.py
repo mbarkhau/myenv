5affee36c308dfd09257a8594570ae57f5eccee7
Manuel Barkhau <mbarkhau@gmail.com>
2018-11-30 15:42:30 +0100

    add config parsing and simplify instantiation

--- src/myenv/__init__.py
+++ src/myenv/__init__.py
@@ -3,6 +3,27 @@
 #
 # (C) 2018 Manuel Barkhau (@mbarkhau)
 # SPDX-License-Identifier: MIT
+"""Environment variable parsing using type annotations.
+
+Usage:
+
+>>> import os
+>>> os.environ['CREDENTIALS_USER'] = "franz"
+>>> os.environ['CREDENTIALS_KEY'] = "supersecret"
+>>>
+>>> class Credentials(BaseEnv):
+...     _environ_prefix = "CREDENTIALS_"
+...     user : str = "user"
+...     key : str
+...
+>>> creds = Credentials()
+>>> creds.user
+'franz'
+>>> creds.key
+'supersecret'
+>>> Credentials() is Credentials()  # singleton
+True
+"""
 
 import os
 import typing as typ
@@ -12,104 +33,202 @@ import pathlib as pl
 __version__ = "v201809.0002-beta"
 
 
-class BaseEnv:
-
-    def __init__(self, **kwargs):
-        for key, val in kwargs.items():
-            setattr(self, key, val)
-
-    def _asdict(self):
-        pass
+Environ = typ.MutableMapping[str, str]
 
 
-__default_sentinel__ = '__default_sentinel__'
+EnvType = typ.TypeVar('EnvType', bound='BaseEnv')
 
 
-Environ = typ.Mapping[str, str]
+def _iter_env_config(env_name: str) -> typ.Iterable[typ.Tuple[str, str]]:
+    env_config_dir = os.getenv('ENV_CONFIG_DIR', ".")
+    CONFIG_DIR     = pl.Path(env_config_dir) / "config"
+    config_files   = [CONFIG_DIR / (env_name + ".env")]
+    if env_name != 'prod':
+        config_files.append(CONFIG_DIR / "prod.env")
 
-EnvType = typ.TypeVar('EnvType', bound=BaseEnv)
+    for config_file in config_files:
+        if not config_file.exists():
+            continue
+
+        with config_file.open(mode="rt", encoding="utf-8") as fh:
+            config_lines = fh.readlines()
+
+        for line in config_lines:
+            if "=" in line and not line.startswith("#"):
+                name, value = line.strip().split("=", 1)
+                yield name.strip(), value
 
 
-def iter_fields(
-    env_type: typ.Type[EnvType], prefix: str
-) -> typ.Iterable[typ.Tuple[str, str, typ.Any, typ.Any]]:
-    for field_name, field_type in env_type.__annotations__.items():
-        default = getattr(env_type, field_name, __default_sentinel__)
-        env_key = (prefix + field_name).upper()
-        yield field_name, env_key, field_type, default
+_environ_initialized: typ.Set[int] = set()
 
 
-def parse_val(val: str, ftype: typ.Any) -> typ.Any:
+def _init_environ(environ: Environ = None) -> None:
+    if id(environ) in _environ_initialized:
+        return
+
+    if environ is None:
+        environ = os.environ
+
+    env_name = os.getenv('ENV', 'prod').lower()
+
+    for name, value in _iter_env_config(env_name):
+        if name not in environ:
+            environ[name] = value.strip()
+
+    _environ_initialized.add(id(environ))
+
+
+__fallback_sentinel__ = '__fallback_sentinel__'
+
+
+FieldType  = typ.Any
+FieldValue = typ.Any
+
+
+class _Field(typ.NamedTuple):
+
+    fname   : str
+    ftyp    : FieldType
+    env_key : str
+    fallback: FieldValue
+
+
+def _iter_fields(env_type: typ.Type[EnvType]) -> typ.Iterable[_Field]:
+    prefix = env_type._environ_prefix or ""
+    for fname, ftyp in env_type.__annotations__.items():
+        fallback = getattr(env_type, fname, __fallback_sentinel__)
+        env_key  = (prefix + fname).upper()
+        yield _Field(fname, ftyp, env_key, fallback)
+
+
+def _parse_bool(val: str) -> bool:
+    if val.lower() in ("1", "true"):
+        return True
+    elif val.lower() in ("0", "false"):
+        return False
+    else:
+        raise ValueError(val)
+
+
+def _parse_list_val(val: str, ftype: FieldType) -> typ.List[FieldValue]:
+    list_strvals = [strval for strval in val.split(os.pathsep)]
+    if ftype.__args__ == (str,):
+        return list_strvals
+    elif ftype.__args__ == (pl.Path,):
+        return [pl.Path(listval) for listval in list_strvals]
+    elif ftype.__args__ == (int,):
+        return [int(listval, 10) for listval in list_strvals]
+    elif ftype.__args__ == (float,):
+        return [float(listval) for listval in list_strvals]
+    elif ftype.__args__ == (bool,):
+        return [_parse_bool(listval) for listval in list_strvals]
+    else:
+        raise TypeError(ftype)
+
+
+def _parse_val(val: str, ftype: FieldType) -> FieldValue:
     if ftype == str:
         return val
     elif ftype == bool:
-        if val.lower() in ("1", "true"):
-            return True
-        elif val.lower() in ("0", "false"):
-            return False
-        else:
-            raise ValueError(val)
+        return _parse_bool(val)
     elif ftype == int:
         return int(val, 10)
     elif ftype == float:
         return float(val)
     elif ftype == pl.Path:
         return pl.Path(val)
-    elif ftype._name in ('List', 'Set'):
-        list_strvals = [strval for strval in val.split(os.pathsep)]
-        if ftype.__args__ == (str,):
-            list_val = list_strvals
-        elif ftype.__args__ == (pl.Path,):
-            list_val = [pl.Path(listval) for listval in list_strvals]
-        elif ftype.__args__ == (int,):
-            list_val = [int(listval, 10) for listval in list_strvals]
-        elif ftype.__args__ == (float,):
-            list_val = [float(listval) for listval in list_strvals]
-        elif ftype.__args__ == (bool,):
-            list_val = []
-            for listval in list_strvals:
-                if listval.lower() in ("1", "true"):
-                    list_val.append(True)
-                elif listval.lower() in ("0", "false"):
-                    list_val.append(False)
-                else:
-                    raise ValueError(listval)
-        else:
-            raise TypeError(ftype)
-
-        if ftype._name == 'List':
-            return list_val
-        elif ftype._name == 'Set':
-            return set(list_val)
-        else:
-            # This cannot happen
-            raise TypeError(ftype)
+    elif ftype.__name__ == 'List':
+        return _parse_list_val(val, ftype)
+    elif ftype.__name__ == 'Set':
+        return set(_parse_list_val(val, ftype))
     elif callable(ftype):
         return ftype(val)
     else:
         raise TypeError(ftype)
 
 
-def parse(
-    env_type: typ.Type[EnvType], environ: Environ = os.environ, prefix: str = None
-) -> EnvType:
-    if prefix is None:
-        prefix = getattr(env_type, '_environ_prefix', "")
+# Cache for already loaded environment configs. Environment
+# variables are only parsed once during initialization.
 
-    typename = env_type.__name__
-    kwargs: typ.MutableMapping[str, typ.Any] = {}
-    for fname, env_key, ftype, default in iter_fields(env_type, prefix):
-        if env_key in environ:
-            try:
-                raw_env_val = environ[env_key]
-                kwargs[fname] = parse_val(raw_env_val, ftype)
-            except ValueError as err:
-                raise ValueError(
-                    f"Invalid value '{raw_env_val}' for {env_key}. "
-                    f"Attepmted to parse '{typename}.{fname}' with '{ftype}'."
+EnvMapKey = typ.Tuple[typ.Type[EnvType], int]
+EnvMap    = typ.Dict[EnvMapKey, EnvType]
+
+_envmap: EnvMap = {}
+
+
+class _Singleton(type):
+    def __call__(cls, *args, **kwargs) -> EnvType:
+        env_cls = typ.cast(typ.Type[EnvType], cls)
+        environ: Environ = kwargs.get('environ', os.environ)
+        envmap_key = (env_cls, id(environ))
+
+        if envmap_key not in _envmap:
+            _init_environ(environ)
+            _envmap[envmap_key] = env_cls.__new__(env_cls, environ=environ)
+
+        return _envmap[envmap_key]
+
+
+class BaseEnv(metaclass=_Singleton):
+    """The main Base class.
+
+    Subclasses of BaseEnv are only instantiated once (singleton).
+    """
+
+    _environ_prefix: typ.Optional[str] = None
+
+    def __new__(cls: typ.Type[EnvType], **kwargs) -> EnvType:
+        """Create a new env instance.
+
+        This should not be called from outside of myenv.
+        """
+        environ: Environ = kwargs['environ']
+        typename = cls.__name__
+        init_kwargs: typ.MutableMapping[str, typ.Any] = {}
+        for field in _iter_fields(cls):
+            if field.env_key in environ:
+                try:
+                    raw_env_val = environ[field.env_key]
+                    init_kwargs[field.fname] = _parse_val(raw_env_val, field.ftyp)
+                except ValueError as err:
+                    raise ValueError(
+                        f"Invalid value '{raw_env_val}' for {field.env_key}. "
+                        f"Attepmted to parse '{typename}.{field.fname}' with '{field.ftyp}'.",
+                        err,
+                    )
+            elif field.fallback != __fallback_sentinel__:
+                init_kwargs[field.fname] = field.fallback
+            else:
+                raise KeyError(
+                    f"No environment variable {field.env_key} "
+                    + f"found for field {typename}.{field.fname}"
                 )
-        elif default != __default_sentinel__:
-            kwargs[fname] = default
-        else:
-            raise KeyError(f"No environment variable {env_key} found for field {typename}.{fname}")
-    return env_type(**kwargs)
+
+        env = super(BaseEnv, cls).__new__(cls)
+        env.__init__(**init_kwargs)
+        return env
+
+    def __init__(self, **kwargs) -> None:
+        for key, val in kwargs.items():
+            setattr(self, key, val)
+
+    def _varnames(self) -> typ.List[str]:
+        prefix = self._environ_prefix or ""
+        return [
+            (prefix + attrname).upper()
+            for attrname in type(self).__annotations__
+            if not attrname.startswith("_")
+        ]
+
+    def _asdict(self):
+        pass
+
+
+def parse(env_type: typ.Type[EnvType], environ: Environ = os.environ) -> EnvType:
+    """Create an instance of an env.
+
+    This is depricated, just instantiate like a normal class
+    - myenv.parse(MyEnv)
+    + MyEnv()
+    """
+    return env_type(environ=environ)
--- test/test_myenv.py
+++ test/test_myenv.py
@@ -1,9 +1,12 @@
+import os
 import myenv
 import pathlib as pl
 import typing as typ
 
 
 class DBEnv(myenv.BaseEnv):
+
+    _environ_prefix = "MYAPP_DB_"
 
     host     : str = "127.0.0.1"
     port     : int = 5432
@@ -24,7 +27,14 @@ def test_parse():
         'MYAPP_DB_READ_ONLY': "0",
         'MYAPP_DB_DDL'      : "~/mkdb.sql",
     }
-    dbenv = myenv.parse(DBEnv, environ, prefix="MYAPP_DB_")
+    environ_before = str(os.environ)
+
+    dbenv = DBEnv(environ=environ)
+
+    environ_after = str(os.environ)
+    assert environ_before == environ_after
+
+    assert set(dbenv._varnames()) == set(environ.keys())
 
     assert dbenv.host     == "1.2.3.4"
     assert dbenv.port     == 1234
@@ -35,12 +45,18 @@ def test_parse():
     assert isinstance(dbenv.ddl, pl.Path)
     assert str(dbenv.ddl).endswith("mkdb.sql")
 
-    attrnames = [attrname for attrname in dir(dbenv) if not attrname.startswith("__")]
+    attrnames = [attrname for attrname in dbenv.__annotations__ if not attrname.startswith("_")]
     assert len(attrnames) == 7
 
 
 def test_defaults():
-    dbenv = myenv.parse(DBEnv, environ={'PASSWORD': "secret"})
+    environ_before = str(os.environ)
+
+    dbenv = DBEnv(environ={'MYAPP_DB_PASSWORD': "secret"})
+
+    environ_after = str(os.environ)
+    assert environ_before == environ_after
+
     assert dbenv.host     == "127.0.0.1"
     assert dbenv.port     == 5432
     assert dbenv.name     == "app_db_v1"
@@ -51,7 +67,7 @@ def test_defaults():
     assert isinstance(dbenv.ddl, pl.Path)
     assert str(dbenv.ddl).endswith("create_tables.sql")
 
-    attrnames = [attrname for attrname in dir(dbenv) if not attrname.startswith("__")]
+    attrnames = [attrname for attrname in dbenv.__annotations__ if not attrname.startswith("_")]
     assert len(attrnames) == 7
 
 
@@ -65,6 +81,8 @@ class TestEnv(myenv.BaseEnv):
     bool_val            : bool = True
     float_val_wo_default: float
     float_val           : float = 12.34
+    strs_val_wo_default : typ.List[str]
+    strs_val            : typ.List[str] = ['bar']
     path_val_wo_default : pl.Path
     path_val            : pl.Path = pl.Path("file.txt")
     paths_val_wo_default: typ.List[pl.Path]
@@ -77,13 +95,13 @@ class TestEnv(myenv.BaseEnv):
 
 def test_errors():
     try:
-        dbenv = myenv.parse(TestEnv, {})
+        dbenv = TestEnv(environ={})
         assert False
     except KeyError as err:
         assert "STR_VAL_WO_DEFAULT" in str(err)
 
     try:
-        dbenv = myenv.parse(TestEnv, {'STR_VAL_WO_DEFAULT': "bar"})
+        dbenv = TestEnv(environ={'STR_VAL_WO_DEFAULT': "bar"})
         assert False
     except KeyError as err:
         assert "INT_VAL_WO_DEFAULT" in str(err)
@@ -93,51 +111,53 @@ def test_errors():
         'INT_VAL_WO_DEFAULT'  : "123",
         'BOOL_VAL_WO_DEFAULT' : "TRUE",
         'FLOAT_VAL_WO_DEFAULT': "123.456",
+        'STRS_VAL_WO_DEFAULT' : "baz:buz",
         'PATH_VAL_WO_DEFAULT' : "fileA.txt",
         'PATHS_VAL_WO_DEFAULT': "fileA.txt:fileB.txt:fileC.txt",
     }
-    dbenv = myenv.parse(TestEnv, good_environ)
+    dbenv = TestEnv(environ=good_environ)
+
+    assert dbenv.str_val             == "foo"
+    assert dbenv.str_val_wo_default  == "bar"
+    assert dbenv.strs_val            == ['bar']
+    assert dbenv.strs_val_wo_default == ["baz", 'buz']
 
     try:
         bad_environ = good_environ.copy()
         bad_environ['INT_VAL'] = "abc"
-        dbenv = myenv.parse(TestEnv, bad_environ)
+        dbenv = TestEnv(environ=bad_environ)
         assert False
     except ValueError as err:
         assert 'INT_VAL' in str(err)
         assert 'abc' in str(err)
-        assert 'TestEnv.int_val' in str(err)
+        assert "TestEnv.int_val" in str(err)
 
     try:
         bad_environ = good_environ.copy()
         bad_environ['INT_VAL'] = "123af"
-        dbenv = myenv.parse(TestEnv, bad_environ)
+        dbenv = TestEnv(environ=bad_environ)
         assert False
     except ValueError as err:
         assert 'INT_VAL' in str(err)
         assert '123af' in str(err)
-        assert 'TestEnv.int_val' in str(err)
+        assert "TestEnv.int_val" in str(err)
 
     try:
         bad_environ = good_environ.copy()
         bad_environ['BOOL_VAL'] = "yes"
-        dbenv = myenv.parse(TestEnv, bad_environ)
+        dbenv = TestEnv(environ=bad_environ)
         assert False
     except ValueError as err:
         assert 'BOOL_VAL' in str(err)
         assert 'yes' in str(err)
-        assert 'TestEnv.bool_val' in str(err)
+        assert "TestEnv.bool_val" in str(err)
 
     try:
         bad_environ = good_environ.copy()
         bad_environ['FLOAT_VAL'] = "abc.23"
-        dbenv = myenv.parse(TestEnv, bad_environ)
+        dbenv = TestEnv(environ=bad_environ)
         assert False
     except ValueError as err:
         assert 'FLOAT_VAL' in str(err)
-        assert 'abc.23' in str(err)
-        assert 'TestEnv.float_val' in str(err)
-
-
-# def test_paths():
-#     assert False
+        assert "abc.23" in str(err)
+        assert "TestEnv.float_val" in str(err)
